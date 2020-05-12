f0fea2302c0236faf3f3df1b26260f3f02ccb919
ak5123 <kanduarul@gmail.com>
2020-05-12 22:10:08 +0530

    edited findOptimal

--- components/compact.js
+++ components/compact.js
@@ -13,53 +13,38 @@ const _lzutf8Decompress = R.curry(lzutf8.decompress)(R.__, { inputEncoding: 'Buf
 // Decompress a buffer using LZ decompression
 const decompress = R.pipe(Buffer.from, _lzutf8Decompress)
 
-// returns optimal zwcs for 2nd level compression
+// returns optimal zwcs through RLE for 2nd level compression
 const findOptimal = (secret,zwc) => {
-    let rep = {}
-    zwc.forEach((ele) => {
-      rep[ele] = {}
-    })
-    console.log("Secret len",secret.length)
-    let size = secret.length
-    for (let j = 0; j<size ; j++) {
+    let dict = zwc.reduce((acc,data) => {
+      acc[data] = {}
+      return acc
+    },{})
+    const size = secret.length
+    for (let j = 0; j < size ; j++) {
       let count = 1
-      while(j<size && secret[j] == secret[j+1]){
-        count ++;
+      while(j < size && secret[j] == secret[j+1]){
+        count++;
         j++;
       }
       if (count >= 2) {
         let itr = count
         while (itr >= 2) {
-          let points = Math.floor(count/itr)*(itr-1)
-          if (rep[secret[j]][itr]) {
-            rep[secret[j]][itr] += points
-          } else {
-            rep[secret[j]][itr] = points
-          }
+          dict[secret[j]][itr] = (dict[secret[j]][itr] || 0) + Math.floor(count/itr)*(itr-1)
           itr--
         }
       }
     }
-    let getMax = []
-    for (let key in rep) {
-      for (let reps in rep[key]) {
-        getMax.push([key+reps,rep[key][reps]])
+    const getOptimal = []
+    for (let key in dict) {
+      for (let count in dict[key]) {
+        getOptimal.push([key+count,dict[key][count]])
       }
     }
-    getMax.sort((a,b) => {
+    getOptimal.sort((a,b) => {
       return b[1] - a[1]
     })
-    console.log('reduce secret len:',getMax)
-    let res = []
-    for (let val of getMax) {   
-      if (val[0][1] == '2') {
-        res.push(val[0][0])
-        if (res.length == 2)
-          break
-      }   
-    }
-    res.sort()
-    return res
+    const reqZwc = getOptimal.filter((val) => val[0][1] == '2').slice(0,2).map(zwc => zwc[0][0])
+    return reqZwc.sort()
   }
 
 
