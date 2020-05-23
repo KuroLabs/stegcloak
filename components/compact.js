1fe330918448b877c6cb602cd7c165849b27e1ff
JyothishmathiCV <cvjyothishmathi@gmail.com>
2020-05-23 09:07:51 +0530

    New ZWC compression added

--- components/compact.js
+++ components/compact.js
@@ -48,33 +48,33 @@ const findOptimal = (secret, characters) => {
   }
   const rankedTable = R.sort((a, b) => b[1] - a[1], getOptimal)
 
-  let reqZwc = rankedTable.filter((val) => val[0][1] === '2').slice(0, 2).map(chars => chars[0][0])
+  let reqZwc = rankedTable.filter((val) => val[0][1] === '2').slice(0, 3).map(chars => chars[0][0])
 
-  if (reqZwc.length !== 2) {
-    reqZwc = reqZwc.concat(R.difference(secret, reqZwc).slice(0, 2 - reqZwc.length))
+  if (reqZwc.length !== 3) {
+    reqZwc = reqZwc.concat(R.difference(secret, reqZwc).slice(0, 3 - reqZwc.length))
   }
 
   return reqZwc.slice().sort()
 }
 
 const zwcHuffMan = (zwc) => {
-  const tableMap = [zwc[0] + zwc[1], zwc[0] + zwc[2], zwc[0] + zwc[3], zwc[1] + zwc[2], zwc[1] + zwc[3], zwc[2] + zwc[3]]
+  const tableMap = [zwc[0] + zwc[1] + zwc[2], zwc[0] + zwc[1] + zwc[3], zwc[0] + zwc[2] + zwc[3], zwc[1] + zwc[2] + zwc[3]]
 
-  const _getCompressFlag = (zwc1, zwc2) => zwc[tableMap.indexOf(zwc1 + zwc2)] // zwA,zwB => zwC
+  const _getCompressFlag = (zwc1, zwc2, zwc3) => zwc[tableMap.indexOf(zwc1 + zwc2 + zwc3)] // zwA,zwB,zwcC => zwD
 
-  const _extractCompressFlag = zwc1 => tableMap[zwc.indexOf(zwc1)].split('') // zwC => zwA,zwcB
+  const _extractCompressFlag = zwc1 => tableMap[zwc.indexOf(zwc1)].split('') // zwcD => zwA,zwcB,zwcC
 
   const shrink = (secret) => {
     const repeatChars = findOptimal(secret, zwc)
 
-    return _getCompressFlag(...repeatChars) + secret.replace(new RegExp(repeatChars[0] + repeatChars[0], 'g'), zwc[4]).replace(new RegExp(repeatChars[1] + repeatChars[1], 'g'), zwc[5])
+    return _getCompressFlag(...repeatChars) + secret.replace(new RegExp(repeatChars[0] + repeatChars[0], 'g'), zwc[4]).replace(new RegExp(repeatChars[1] + repeatChars[1], 'g'), zwc[5]).replace(new RegExp(repeatChars[2] + repeatChars[2], 'g'), zwc[6])
   }
 
   const expand = (secret) => {
     const flag = secret[0]
     const invisibleStream = secret.slice(1)
     const repeatChars = _extractCompressFlag(flag)
-    return invisibleStream.replace(new RegExp(zwc[4], 'g'), repeatChars[0] + repeatChars[0]).replace(new RegExp(zwc[5], 'g'), repeatChars[1] + repeatChars[1])
+    return invisibleStream.replace(new RegExp(zwc[4], 'g'), repeatChars[0] + repeatChars[0]).replace(new RegExp(zwc[5], 'g'), repeatChars[1] + repeatChars[1]).replace(new RegExp(zwc[6], 'g'), repeatChars[2] + repeatChars[2])
   }
 
   return {
